a93b86c2484ca3e41f6c097b03bf8ff213a62297
Anggia Intan <intananggia23@gmail.com>
2024-11-21 22:14:29 +0700

    membuat tampilan klausul persetujuan dengan checkbox yang hanya bisa dicentang jika opsi dropdown dan kelas telah dipilih

--- app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
@@ -143,12 +144,25 @@ Image(
                             onClick = {pilihanKelas = data}
                         )
                         Text(data)
-                    })
+                    }
+                }
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(text = "Klausul Persetujuan Mahasiswa", fontWeight = FontWeight.Bold)
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = {checked = it},
+                        enabled = chosenDropdown.isNotBlank() && pilihanKelas.isNotBlank()
+                    )
+                    Text(
+                        text = "Saya menyetujui setiap pernyataan yang ada tanpa ada paksaan dari pihak manapun.",
+                        fontWeight = FontWeight.Light, fontSize = 10.sp
+                    )
                 }
 
-
-
-                )
             }
         }
 
