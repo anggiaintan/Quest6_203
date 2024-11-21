ee1fc3b0e4a020e93e2905c66feb76a2cb2d7890
Anggia Intan <intananggia23@gmail.com>
2024-11-21 21:46:45 +0700

    membuat Column dengan beberapa elemen yaitu teks dan spasi

--- app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
@@ -95,7 +95,22 @@ Image(
                     )
                 )
                 .fillMaxSize(),
-        )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ){
+                Text(text = "Pilih Mata Kuliah Peminatan", fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Silahkan pilih mata kuliah yang Anda inginkan",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                )
+            }
+        }
 
     }
 
