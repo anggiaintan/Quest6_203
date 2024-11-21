37046be3283a2287d5551ceb5da75c168331264a
Anggia Intan <intananggia23@gmail.com>
2024-11-21 22:04:29 +0700

    membuat sebuah tampilan yang memungkinkan pengguna untuk memilih kelas dengan memilih salah satu radio button yang ada

--- app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
@@ -2,6 +2,7 @@ package com.example.navigationwithdata.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -27,10 +29,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.data.EmptyGroup.data
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.navigationwithdata.R
 import com.example.navigationwithdata.data.MataKuliah
+import com.example.navigationwithdata.data.RuangKelas
 import com.example.navigationwithdata.model.Mahasiswa
 import com.example.navigationwithdata.ui.view.widget.DynamicSelectTextField
 
@@ -128,6 +132,19 @@ Image(
                     fontSize = 12.sp,
                     fontWeight = FontWeight.Light)
                 Spacer(modifier = Modifier.padding(8.dp))
+                Row(
+                modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){
+                    RuangKelas.kelas.forEach( data ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        RadioButton(
+                            selected = pilihanKelas == data,
+                            onClick = {pilihanKelas = data}
+                        )
+                        Text(data)
+                    })
+                }
 
 
 
