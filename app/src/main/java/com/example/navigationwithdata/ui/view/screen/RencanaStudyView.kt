80979c77c035dcbf69d184afd19d345b5d949968
Anggia Intan <intananggia23@gmail.com>
2024-11-21 20:49:07 +0700

    membuat function RencanaStudyView

---
+++ app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
@@ -0,0 +1,13 @@
+package com.example.navigationwithdata.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.example.navigationwithdata.model.Mahasiswa
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+
+}
