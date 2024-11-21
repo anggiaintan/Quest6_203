0753a80cabf26341b725df2068e135ac0e1d6942
Anggia Intan <intananggia23@gmail.com>
2024-11-21 14:09:01 +0700

    membuat objek MataKuliah di dalam package data untuk menyimpan daftar mata kuliah

---
+++ app/src/main/java/com/example/navigationwithdata/data/MataKuliah.kt
@@ -0,0 +1,10 @@
+package com.example.navigationwithdata.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision")}
