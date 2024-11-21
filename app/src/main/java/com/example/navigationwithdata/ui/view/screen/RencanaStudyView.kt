40dfb445ac29f3adfd56af16630a9bbf0078c115
Anggia Intan <intananggia23@gmail.com>
2024-11-21 21:03:16 +0700

    menambahkan sebuah layout Column dan Row di dalam fungsi RencanaStudyView

--- app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationwithdata/ui/view/screen/RencanaStudyView.kt
@@ -1,6 +1,19 @@
 package com.example.navigationwithdata.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.navigationwithdata.R
 import com.example.navigationwithdata.model.Mahasiswa
 
 @Composable
@@ -17,5 +30,18 @@ var chosenDropDown by remember {
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(chosenDropDown, pilihanKelas)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)),
+    ){
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+        ){
+
+        }
+    }
 
 }
