869ea406e30987803363a260eb3686b688ead109
Anggia Intan <intananggia23@gmail.com>
2024-11-21 14:23:22 +0700

    membuat fungsi DynamicSelectTextField untuk membuat kolom input teks yang bisa memilih nilai dari daftar pilihan (options)

---
+++ app/src/main/java/com/example/navigationwithdata/ui/view/widget/DynamicSelectedField.kt
@@ -0,0 +1,16 @@
+package com.example.navigationwithdata.ui.view.widget
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DynamicSelectTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+
+){
+
+}
